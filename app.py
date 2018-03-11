2c6c04bc661ac71f0920cf82ec2ad47ca19dbf2d
nithyadhana <nithi1003@gmail.com>
2018-03-11 16:37:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 02 10:26:53 2018
+
+@author: nithya
+"""
+
 #!/usr/bin/env python
 
 import urllib
 import json
 import os
 import csv
-import pandas as pd
-from var import UnQstn, ArrayOfQuestions
-UQ = UnQstn("","","","")
 
 from flask import Flask
 from flask import request
@@ -37,14 +41,12 @@ def makeWebhookResult(req):
         result = req.get("result")
         parameters = result.get("parameters")
         zone = parameters.get("subject_name")
-    
         csv_file = csv.reader(open('books1.csv'), delimiter=",")
-       
-        for row in csv_file:    
-            if zone == row[1]:
-                speech = ("\n\nBook Id: " + row[0] + "\n Book Title: " + row[1] + "\n Authors: " + row[2] + "\n Publication: " + row[3] + "\n Rack Number:" + row[4])
         
-        print(speech)
+        for row in csv_file:
+            if zone == row[1]:
+                speech = ("Book Id: " + row[0] + "\n Book Title: " + row[1] + "\n Authors: " + row[2] + "\n Publication: " + row[3] + "\n Rack Number: "+ row[4])
+
         
         
         print("Response:")
@@ -57,71 +59,9 @@ def makeWebhookResult(req):
                 "source": "input-subject-name"
                }
     
-    elif req.get("result").get("action") == "input-publication":
-        
-        result = req.get("result")
-        parameters = result.get("parameters")
-        zone = parameters.get("publication_names")
     
-        csv_file = csv.reader(open('books1.csv'), delimiter=",")
-       
-        for row in csv_file:    
-            if zone == row[3]:
-                speech = ("\n\nBook Id: " + row[0] + "\n Book Title: " + row[1] + "\n Authors: " + row[2] + "\n Publication: " + row[3] + "\n Rack Number:" + row[4])
-        
-        print(speech)
-        
-        
-        print("Response:")
-        print(speech)
-        return {
-                "speech": speech,
-                "displayText": speech,
-                #"data": {},
-                #"contextOut": [],
-                "source": "input-publication"
-               }
+      
     
-    elif req.get("result").get("action") == "issue-input":
-        
-        result = req.get("result")
-        parameters = result.get("parameters")
-        pub_name = parameters.get("publication_names")
-        sub_name = parameters.get("subject_name")
-        user_id = parameters.get("user_id")
-        status = "yes"
-        yes = "yes"
-        no = "no"
-    
-        csv_file = csv.reader(open('issue.csv'), delimiter=",")
-        
-        for row in csv_file:
-            if user_id == row[0] and no == row[12]:
-        
-         
-                UQ.user = user_id
-                UQ.question = sub_name
-                UQ.answer = pub_name
-                UQ.status = status
-                print("\n You are eligible to issue books")
-    
-    
-                with open('issue1.csv', 'a') as Ques_Ans:
-                    writer = csv.writer(Ques_Ans)
-                    writer.writerow([UQ.user,UQ.question, UQ.answer,UQ.status])    
-                    print("\nThe Book has been issued to your name!")
-   
-        
-        
-        print("Response:")
-        print(speech)
-        return {
-                "speech": speech,
-                "displayText": speech,
-                #"data": {},
-                #"contextOut": [],
-                "source": "issue-input"
-               }
     
         
     
