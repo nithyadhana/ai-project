e1b291bada6893b8d1a2b3091ac94bfe2a63c505
nithyadhana <nithi1003@gmail.com>
2018-03-19 17:59:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -37,21 +37,9 @@ def makeWebhookResult(req):
         csv_file = csv.reader(open('books1.csv'), delimiter=",")
        
         for row in csv_file:
-            if row[1] == "advanced computer architecutre":
+            if zone == row[1]:
                 speech = ("\n\nBook Id: " + row[0] + "\n Book Title: " + row[1] + "\n Authors: " + row[2] + "\n Publication: " + row[3] + "\n status:" + row[5] + " \n Rack Number:" + row[4])
-        
                 print(speech)
-        
-        
-                print("Response:")
-                print(speech)
-                return {
-                    "speech": speech,
-                    "displayText": speech,
-                        #"data": {},
-                        #"contextOut": [],
-                    "source": "input-subject-name"
-                        }
     
       elif req.get("result").get("action") == "input-publication":
         
