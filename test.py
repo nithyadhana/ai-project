5851df22b1527bcebe30253ea7d652174e3111a8
nithyadhana <nithi1003@gmail.com>
2018-03-29 19:46:05 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ Created on Wed Feb 28 15:17:06 2018
 """
 import csv
 csv_file = csv.reader(open('books1.csv'), delimiter=",")
-zone = "1"        
+zone = "computer networks"        
 for row in csv_file:
-    if zone == row[0]:
+    if zone == row[1]:
         speech = ("\n\nBook Id: " + row[0] + "\n Book Title: " + row[1] + "\n Authors: " + row[2] + "\n Publication: " + row[3] + "\n Rack Number:" + row[4])
         
         print(speech)
